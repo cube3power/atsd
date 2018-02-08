f885c7f87c69dfdb09d14adb184bed8018071547
Anna Striganova <snowswim@mail.ru>
2018-02-08 11:54:03 +0300

    url_avail.sh updates (#407)

--- rule-engine/resources/url_avail.sh
+++ rule-engine/resources/url_avail.sh
@@ -2,15 +2,19 @@
 
 url=${1}
 dir=$(dirname $(readlink -f $0))
-status=$(curl -sS --insecure -X GET -m 10 -D ${dir}/headers -w "\nResponse Time: %{time_total}\n" "${url}" > ${dir}/response 2>&1)
+status=$(curl -sS -L --insecure -X GET -m 10 -D ${dir}/headers -w "\nResponse Time: %{time_total}\n" "${url}" > ${dir}/response 2>&1)
 if [[ $? == 0 ]] ; then
   code=$(head -n 1 ${dir}/headers | grep -oiE "[0-9]{3}[a-z ]*")
   echo "Status code: ${code}"
   echo "$(tail -n 1 ${dir}/response)" # Response Time
   length=$(head -n -1 ${dir}/response | wc -c)
   echo "Content Length: ${length} bytes"
+  grep "Location:" ${dir}/headers
 else
   head -n 1 ${dir}/response
 fi
 
 rm  ${dir}/response  ${dir}/headers
+
+
+
